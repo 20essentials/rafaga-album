4ca94c6b0740b7d95d6c381f3b952489658360c7
DominicDev <s0l0estudi4r@gmail.com>
2026-03-28 13:54:15 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'RÁFAGA';
+export let nameSongs = [
+	"UNA CERVEZA", /*1*/
+	"MENTIROSA", /*2*/
+	"UNA RÁFAGA DE AMOR", /*3*/
+	"MALDITO CORAZÓN", /*4*/
+	"LA LUNA Y TÚ", /*5*/
+	"AGÜITA", /*6*/
+	"MUERO DE FRÍO", /*7*/
+	"TAN SOLO UN MINUTO", /*8*/
+	"VENGO A GRITAR QUE TE AMO", /*9*/
+	"TÚ", /*10*/
+	"NO COMPRENDÍ TU AMOR", /*11*/
+	"EXPLÍCAME", /*12*/
+	"BRINDO POR ELLA", /*13*/
+	"NO SÉ VIVIR", /*14*/
+	"TU TRAICIÓN", /*15*/
+	"BAILA PARA MI", /*16*/
+	"QUISIERA CONTARTE", /*17*/
+	"PENSANDO EN TI", /*18*/
+	"QUÉDATE AMOR", /*19*/
+	"MENTIRAS, ENGAÑOS", /*20*/
+]
